af8e2f870ec37a640dfca9c6ab9f30aed53471db
spalewski@gmail.com <spalewski@gmail.com>
2018-02-13 15:18:59 +0100

    Day9

--- src/com/company/daysOfCode/Day9.java
+++ src/com/company/daysOfCode/Day9.java
@@ -38,15 +38,15 @@ import java.util.Scanner;
 
 public class Day9 {
 
-
- //   static int factorial(int n) {
-        // Complete this function
-   // }
+    static int factorial(int n) {
+        if (n == 1) return 1;
+        return n * factorial(n - 1);
+    }
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
-       // int result = factorial(n);
-       // System.out.println(result);
+        int result = factorial(n);
+        System.out.println(result);
     }
 }
